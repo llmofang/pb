1ea1627a0bb2ca1a1025d2f8b4f1086c4137c6c3
RuoyeSnoer <ruoye_snoer@163.com>
2017-06-21 00:29:59 +0800

    add microserver

--- test.pb.go
+++ test.pb.go
@@ -33,10 +33,9 @@ type PBtest struct {
 	XXX_unrecognized []byte  `json:"-"`
 }
 
-func (m *PBtest) Reset()                    { *m = PBtest{} }
-func (m *PBtest) String() string            { return proto.CompactTextString(m) }
-func (*PBtest) ProtoMessage()               {}
-func (*PBtest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
+func (m *PBtest) Reset()         { *m = PBtest{} }
+func (m *PBtest) String() string { return proto.CompactTextString(m) }
+func (*PBtest) ProtoMessage()    {}
 
 func (m *PBtest) GetTest() string {
 	if m != nil && m.Test != nil {
@@ -47,14 +46,4 @@ func (m *PBtest) GetTest() string {
 
 func init() {
 	proto.RegisterType((*PBtest)(nil), "pb.PBtest")
-}
-
-func init() { proto.RegisterFile("test.proto", fileDescriptor0) }
-
-var fileDescriptor0 = []byte{
-	// 62 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xe2, 0xe2, 0x2a, 0x49, 0x2d, 0x2e,
-	0xd1, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x2a, 0x48, 0x52, 0x92, 0xe1, 0x62, 0x0b, 0x70,
-	0x02, 0x89, 0x09, 0x09, 0x71, 0xb1, 0x80, 0x68, 0x09, 0x46, 0x05, 0x26, 0x0d, 0xce, 0x20, 0x30,
-	0x1b, 0x10, 0x00, 0x00, 0xff, 0xff, 0x64, 0x9c, 0x0b, 0x96, 0x2e, 0x00, 0x00, 0x00,
 }
